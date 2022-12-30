62a7b15c1d2bbffd1d83d0b7692c2b9fe5294316
yourusername <truebaescobaliker@gmail.com>
2022-12-30 19:38:11 +0100

    counter 1

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -3,9 +3,6 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-console.log(value);
-console.log(btns);
-
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         const style = e.currentTarget.classList;
